3a245018db5b0a48fd44c228bc477a989b49fbce
Hassaan1814 <hassaanjaved82@triconv.com>
2023-01-02 10:45:25 +0500

    login issue fixed

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,7 +1,9 @@
 import React, {useContext, useState} from 'react'
+import { useNavigate } from "react-router";
 import noteContext from "../context/notes/noteContext"
 
 const AddNote = () => {
+  let navigate = useNavigate();
     const context = useContext(noteContext);
     const {addNote} = context;
 
@@ -17,68 +19,74 @@ const AddNote = () => {
         setNote({...note, [e.target.name]: e.target.value})
     }
     return (
-      <div className="container my-3">
-        <h2>
-          Add a <span className="text-primary "> TODO </span>Task
-        </h2>
-        <form className="my-3">
-          <div className="mb-3">
-            <label htmlFor="title" className="form-label">
-              Title
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="title"
-              name="title"
-              aria-describedby="emailHelp"
-              value={note.title}
-              onChange={onChange}
-              minLength={5}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="description" className="form-label">
-              Description
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="description"
-              name="description"
-              value={note.description}
-              onChange={onChange}
-              minLength={5}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="tag" className="form-label">
-              Tag
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="tag"
-              name="tag"
-              value={note.tag}
-              onChange={onChange}
-              minLength={5}
-              required
-            />
-          </div>
+      <>
+        {localStorage.getItem("token") ? (
+          <div className="container my-3">
+            <h2>
+              Add a <span className="text-primary "> TODO </span>Task
+            </h2>
+            <form className="my-3">
+              <div className="mb-3">
+                <label htmlFor="title" className="form-label">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="title"
+                  name="title"
+                  aria-describedby="emailHelp"
+                  value={note.title}
+                  onChange={onChange}
+                  minLength={5}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="description" className="form-label">
+                  Description
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="description"
+                  name="description"
+                  value={note.description}
+                  onChange={onChange}
+                  minLength={5}
+                  required
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="tag" className="form-label">
+                  Tag
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="tag"
+                  name="tag"
+                  value={note.tag}
+                  onChange={onChange}
+                  minLength={5}
+                  required
+                />
+              </div>
 
-          <button
-            disabled={note.title.length < 5 || note.description.length < 5}
-            type="submit"
-            className="btn btn-primary"
-            onClick={handleClick}
-          >
-            Add Note
-          </button>
-        </form>
-      </div>
+              <button
+                disabled={note.title.length < 5 || note.description.length < 5}
+                type="submit"
+                className="btn btn-primary"
+                onClick={handleClick}
+              >
+                Add Note
+              </button>
+            </form>
+          </div>
+        ) : (
+          navigate("/login")
+        )}
+      </>
     );
 }
 
